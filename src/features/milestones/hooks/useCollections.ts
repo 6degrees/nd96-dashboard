d7cf1c44b8bbf42697490991e7f263f2e0353fd4
abdroos <abd.alaidrous@gmail.com>
2026-09-20 12:02:43 +0300

    fix: fix sort

--- src/features/milestones/hooks/useCollections.ts
+++ src/features/milestones/hooks/useCollections.ts
@@ -16,6 +16,6 @@ export const useCollections = (timelineId: string) =>
             "filters[created_at][$lte]": filters.createdAt?.[1],
             "filters[updated_at][$gte]": filters.updatedAt?.[0],
             "filters[updated_at][$lte]": filters.updatedAt?.[1],
-            "sort": "sort_order:asc,year:asc",
+            "sort": "sort_order:asc",
         }),
     })()
